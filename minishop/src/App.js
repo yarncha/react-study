db1383d9b6c721a58a27c6c75992e9c84bed33d5
YAMCHA <dbals9812@naver.com>
2021-08-10 13:43:07 +0900

    [210809] TIL

--- minishop/src/App.js
+++ minishop/src/App.js
@@ -5,7 +5,10 @@ import { NavDropdown } from 'react-bootstrap';
 import { Container } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
+import { Link, Route, Switch } from 'react-router-dom';
+
 import { ShoesData } from './data.js';
+import { Detail } from './detailData.js';
 
 function ShoesInfo(props) {
   // 신발의 상세 정보를 출력하는 부분
@@ -31,8 +34,8 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link href="#home"><Link to="/">Home</Link></Nav.Link>
+              <Nav.Link href="#detail"><Link to="/detail">Detail</Link></Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -44,23 +47,32 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <div class="JumbotronDiv">
-        <p class="JumbotronTitle">React Shop</p>
-        <p class="JumbotronDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec diam ac lectus commodo viverra. Mauris erat nunc, efficitur et orci sed, bibendum dignissim mi. Proin fringilla tempor mattis. Fusce pharetra nunc elit. Nullam vehicula enim sit amet tempus tempor. Quisque tincidunt sit amet dui sit amet ultricies. Nam euismod massa vitae mi tincidunt tincidunt. Integer commodo massa metus, non feugiat turpis tempus at.</p>
-        <Button variant="light" size="lg" class="JumbotronButton">Shop
-        </Button>{' '}
-      </div>
-      <div className="container">
-        <div className="row">
-          {
-            shoes.map(function(shoesElement){
-              return(
-                <ShoesInfo shoesElement={shoesElement}></ShoesInfo>
-              );
-            })
-          }
-        </div>
-      </div>
+
+      <Switch>
+        <Route path="/detail">
+          <Detail />
+        </Route>
+
+        <Route path="/">
+          <div class="JumbotronDiv">
+            <p class="JumbotronTitle">React Shop</p>
+            <p class="JumbotronDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec diam ac lectus commodo viverra. Mauris erat nunc, efficitur et orci sed, bibendum dignissim mi. Proin fringilla tempor mattis. Fusce pharetra nunc elit. Nullam vehicula enim sit amet tempus tempor. Quisque tincidunt sit amet dui sit amet ultricies. Nam euismod massa vitae mi tincidunt tincidunt. Integer commodo massa metus, non feugiat turpis tempus at.</p>
+            <Button variant="light" size="lg" class="JumbotronButton">Shop
+            </Button>{' '}
+          </div>
+          <div className="container">
+            <div className="row">
+              {
+                shoes.map(function (shoesElement) {
+                  return (
+                    <ShoesInfo shoesElement={shoesElement}></ShoesInfo>
+                  );
+                })
+              }
+            </div>
+          </div>
+        </Route>
+      </Switch>
     </div>
   );
 }
