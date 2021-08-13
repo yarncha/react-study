f0c723162df0f986ec8814d5020b47be7428f056
YAMCHA <dbals9812@naver.com>
2021-08-13 20:16:55 +0900

    [210813] TIL

--- minishop/src/detailData.js
+++ minishop/src/detailData.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useHistory, useParams } from 'react-router-dom';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import './detailData.scss';
+
 
 let Box = styled.div`
     padding : 20px;
@@ -36,6 +38,10 @@ function Detail(props) {
                     <button className="btn btn-danger">주문하기</button>
                     <button onClick={() => { history.goBack() }} className="btn btn-danger">뒤로가기</button>
                 </div>
+            </div>
+
+            <div className="my-alert">
+                <p>재고가 얼마 남지 않았습니다</p>
             </div>
         </div>
     )
